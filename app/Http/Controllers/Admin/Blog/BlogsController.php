adf6ce5e6d48a364981e24374f0782e8fc9ec7e8
BugTheDebugger <bprayush@yahoo.com>
2020-01-07 19:52:23 +0545

    Creating function prototypes

---
+++ app/Http/Controllers/Admin/Blog/BlogsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Blog;
+
+class BlogsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Function to display the list of BLOGs
+     * @return json
+     */
+    public function index() {
+
+    }
+
+    /**
+     * Function to store new BLOG
+     * @return json
+     */
+    public function store() {
+
+    }
+
+    /**
+     * Funciton to autosave the BLOG
+     */
+    public function autosave() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function update() {
+
+    }
+}
